1a0d7ef61c26ead3e03d1527e364322ad8fb9525
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-05-26 13:57:38 +0200

    add decode tests

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -6,7 +6,7 @@ pub struct TestContext {
 }
 
 // Simple struct with context_type
-#[derive(Encode, Decode, BorrowDecodeFromDecode)]
+#[derive(Debug, Encode, Decode, BorrowDecodeFromDecode, PartialEq)]
 #[trait_decode(context_type = TestContext)]
 pub struct TestStruct {
     pub id: u32,
@@ -14,7 +14,7 @@ pub struct TestStruct {
 }
 
 // Simple enum with context_type
-#[derive(Encode, Decode, BorrowDecodeFromDecode)]
+#[derive(Debug, Encode, Decode, BorrowDecodeFromDecode, PartialEq)]
 #[trait_decode(context_type = TestContext)]
 pub enum TestEnum {
     Simple(u32),
@@ -23,21 +23,62 @@ pub enum TestEnum {
 
 #[test]
 fn test_basic_struct() {
+    let test_context = TestContext { version: 1 };
+
     let test = TestStruct {
         id: 42,
         data: "Hello".to_string(),
     };
-    
+
     // Just test that we can encode
-    let encoded: Vec<u8> = bincode::encode_to_vec(test, bincode::config::standard()).unwrap();
+    let encoded: Vec<u8> = bincode::encode_to_vec(&test, bincode::config::standard()).unwrap();
     assert!(!encoded.is_empty());
+
+    let decoded: TestStruct = bincode::decode_from_slice_with_context(
+        &encoded,
+        bincode::config::standard(),
+        test_context,
+    )
+    .unwrap()
+    .0;
+
+    assert_eq!(decoded, test);
 }
 
 #[test]
 fn test_basic_enum() {
+    let test_context = TestContext { version: 1 };
+
     let test = TestEnum::Simple(42);
-    
+
     // Just test that we can encode
-    let encoded: Vec<u8> = bincode::encode_to_vec(test, bincode::config::standard()).unwrap();
+    let encoded: Vec<u8> = bincode::encode_to_vec(&test, bincode::config::standard()).unwrap();
     assert!(!encoded.is_empty());
+
+    let decoded: TestEnum = bincode::decode_from_slice_with_context(
+        &encoded,
+        bincode::config::standard(),
+        test_context.clone(),
+    )
+    .unwrap()
+    .0;
+
+    assert_eq!(decoded, test);
+
+    let test = TestEnum::Complex {
+        name: "desalniettemin".to_string(),
+    };
+
+    let encoded: Vec<u8> = bincode::encode_to_vec(&test, bincode::config::standard()).unwrap();
+    assert!(!encoded.is_empty());
+
+    let decoded: TestEnum = bincode::decode_from_slice_with_context(
+        &encoded,
+        bincode::config::standard(),
+        test_context,
+    )
+    .unwrap()
+    .0;
+
+    assert_eq!(decoded, test);
 }
